83d05facfe8cb42134e4c92f5c049269a83b7b1d
MayurSharmaMerkle <mayur.sharma@merkle.com>
2025-02-06 18:30:05 +0530

    Update models.py

--- lightweight_mmm/models.py
+++ lightweight_mmm/models.py
@@ -162,56 +162,56 @@ def _get_transform_default_priors() -> Mapping[str, Prior]:
   })
 
 
-# def transform_gamma_adstock(media_data: jnp.ndarray,
-#                       custom_priors: MutableMapping[str, Prior],
-#                       max_lag: int = 13) -> jnp.ndarray:
-#   """Transforms the input data with the gamma adstock function and exponent.
+def transform_gamma_adstock(media_data: jnp.ndarray,
+                      custom_priors: MutableMapping[str, Prior],
+                      max_lag: int = 13) -> jnp.ndarray:
+  """Transforms the input data with the gamma adstock function and exponent.
 
-#   Args:
-#     media_data: Media data to be transformed. It is expected to have 2 dims for
-#       national models and 3 for geo models.
-#     custom_priors: The custom priors we want the model to take instead of the
-#       default ones. The possible names of parameters for gamma adstock and exponent
-#       are "gamma_alpha", "gamma_beta" and "exponent".
+  Args:
+    media_data: Media data to be transformed. It is expected to have 2 dims for
+      national models and 3 for geo models.
+    custom_priors: The custom priors we want the model to take instead of the
+      default ones. The possible names of parameters for gamma adstock and exponent
+      are "gamma_alpha", "gamma_beta" and "exponent".
 
-#   Returns:
-#     The transformed media data.
-#   """
-#   transform_default_priors = _get_transform_default_priors()["gamma_adstock"]
-#   with numpyro.plate(name=f"{_GAMMA_ALPHA}_plate",
-#                      size=media_data.shape[1]):
-#     gamma_alpha = numpyro.sample(
-#         name=_GAMMA_ALPHA,
-#         fn=custom_priors.get(_GAMMA_ALPHA,
-#                              transform_default_priors[_GAMMA_ALPHA]))
+  Returns:
+    The transformed media data.
+  """
+  transform_default_priors = _get_transform_default_priors()["gamma_adstock"]
+  with numpyro.plate(name=f"{_GAMMA_ALPHA}_plate",
+                     size=media_data.shape[1]):
+    gamma_alpha = numpyro.sample(
+        name=_GAMMA_ALPHA,
+        fn=custom_priors.get(_GAMMA_ALPHA,
+                             transform_default_priors[_GAMMA_ALPHA]))
     
-#   with numpyro.plate(name=f"{_GAMMA_BETA}_plate",
-#                      size=media_data.shape[1]):
-#     gamma_beta = numpyro.sample(
-#         name=_GAMMA_BETA,
-#         fn=custom_priors.get(_GAMMA_BETA,
-#                              transform_default_priors[_GAMMA_BETA]))
+  with numpyro.plate(name=f"{_GAMMA_BETA}_plate",
+                     size=media_data.shape[1]):
+    gamma_beta = numpyro.sample(
+        name=_GAMMA_BETA,
+        fn=custom_priors.get(_GAMMA_BETA,
+                             transform_default_priors[_GAMMA_BETA]))
 
-#   with numpyro.plate(name=f"{_EXPONENT}_plate",
-#                      size=media_data.shape[1]):
-#     exponent = numpyro.sample(
-#         name=_EXPONENT,
-#         fn=custom_priors.get(_EXPONENT,
-#                              transform_default_priors[_EXPONENT]))
+  with numpyro.plate(name=f"{_EXPONENT}_plate",
+                     size=media_data.shape[1]):
+    exponent = numpyro.sample(
+        name=_EXPONENT,
+        fn=custom_priors.get(_EXPONENT,
+                             transform_default_priors[_EXPONENT]))
 
-#     gamma_alpha = gamma_alpha*20
-#     gamma_beta = gamma_beta*10
+    gamma_alpha = gamma_alpha*20
+    gamma_beta = gamma_beta*10
 
-#     gamma_adstock = media_transforms.gamma_adstock(
-#       data=media_data, 
-#       gamma_alpha=gamma_alpha, 
-#       gamma_beta=gamma_beta,
-#       max_lag=max_lag)
+    gamma_adstock = media_transforms.gamma_adstock(
+      data=media_data, 
+      gamma_alpha=gamma_alpha, 
+      gamma_beta=gamma_beta,
+      max_lag=max_lag)
     
-#   if media_data.ndim == 3:
-#     exponent = jnp.expand_dims(exponent, axis=-1)
+  if media_data.ndim == 3:
+    exponent = jnp.expand_dims(exponent, axis=-1)
 
-#   return media_transforms.apply_exponent_safe(data=gamma_adstock, exponent=exponent)
+  return media_transforms.apply_exponent_safe(data=gamma_adstock, exponent=exponent)
 
 ####################################################################################################
 
@@ -305,51 +305,51 @@ def _get_transform_default_priors() -> Mapping[str, Prior]:
 
 #######################################################################################################
 
-def transform_gamma_adstock(media_data: jnp.ndarray,
-                      custom_priors: MutableMapping[str, Prior],
-                      max_lag: int = 13) -> jnp.ndarray:
-  """Transforms the input data with the gamma adstock function and exponent.
+# def transform_gamma_adstock(media_data: jnp.ndarray,
+#                       custom_priors: MutableMapping[str, Prior],
+#                       max_lag: int = 13) -> jnp.ndarray:
+#   """Transforms the input data with the gamma adstock function and exponent.
 
-  Args:
-    media_data: Media data to be transformed. It is expected to have 2 dims for
-      national models and 3 for geo models.
-    custom_priors: The custom priors we want the model to take instead of the
-      default ones. The possible names of parameters for gamma adstock and exponent
-      are "gamma_alpha", "gamma_beta" and "exponent".
+#   Args:
+#     media_data: Media data to be transformed. It is expected to have 2 dims for
+#       national models and 3 for geo models.
+#     custom_priors: The custom priors we want the model to take instead of the
+#       default ones. The possible names of parameters for gamma adstock and exponent
+#       are "gamma_alpha", "gamma_beta" and "exponent".
 
-  Returns:
-    The transformed media data.
-  """
-  transform_default_priors = _get_transform_default_priors()["gamma_adstock"]
+#   Returns:
+#     The transformed media data.
+#   """
+#   transform_default_priors = _get_transform_default_priors()["gamma_adstock"]
   
-  with numpyro.plate(name=f"{_GAMMA_ALPHA}_plate", size=media_data.shape[1]):
-    gamma_alpha = numpyro.param(
-        name=_GAMMA_ALPHA,
-        init_value=jnp.ones((media_data.shape[1],)) * custom_priors.get(_GAMMA_ALPHA, transform_default_priors[_GAMMA_ALPHA]).mean)
+#   with numpyro.plate(name=f"{_GAMMA_ALPHA}_plate", size=media_data.shape[1]):
+#     gamma_alpha = numpyro.param(
+#         name=_GAMMA_ALPHA,
+#         init_value=jnp.ones((media_data.shape[1],)) * custom_priors.get(_GAMMA_ALPHA, transform_default_priors[_GAMMA_ALPHA]).mean)
       
-  with numpyro.plate(name=f"{_GAMMA_BETA}_plate", size=media_data.shape[1]):
-    gamma_beta = numpyro.param(
-        name=_GAMMA_BETA,
-        init_value=jnp.ones((media_data.shape[1],)) * custom_priors.get(_GAMMA_BETA, transform_default_priors[_GAMMA_BETA]).mean)
+#   with numpyro.plate(name=f"{_GAMMA_BETA}_plate", size=media_data.shape[1]):
+#     gamma_beta = numpyro.param(
+#         name=_GAMMA_BETA,
+#         init_value=jnp.ones((media_data.shape[1],)) * custom_priors.get(_GAMMA_BETA, transform_default_priors[_GAMMA_BETA]).mean)
 
-  with numpyro.plate(name=f"{_EXPONENT}_plate", size=media_data.shape[1]):
-    exponent = numpyro.param(
-        name=_EXPONENT,
-        init_value=jnp.ones((media_data.shape[1],)) * custom_priors.get(_EXPONENT, transform_default_priors[_EXPONENT]).mean)
+#   with numpyro.plate(name=f"{_EXPONENT}_plate", size=media_data.shape[1]):
+#     exponent = numpyro.param(
+#         name=_EXPONENT,
+#         init_value=jnp.ones((media_data.shape[1],)) * custom_priors.get(_EXPONENT, transform_default_priors[_EXPONENT]).mean)
 
-  gamma_alpha = gamma_alpha*20
-  gamma_beta = gamma_beta*10
+#   gamma_alpha = gamma_alpha*20
+#   gamma_beta = gamma_beta*10
 
-  gamma_adstock = media_transforms.gamma_adstock(
-    data=media_data, 
-    gamma_alpha=gamma_alpha, 
-    gamma_beta=gamma_beta,
-    max_lag=max_lag)
+#   gamma_adstock = media_transforms.gamma_adstock(
+#     data=media_data, 
+#     gamma_alpha=gamma_alpha, 
+#     gamma_beta=gamma_beta,
+#     max_lag=max_lag)
     
-  if media_data.ndim == 3:
-    exponent = jnp.expand_dims(exponent, axis=-1)
+#   if media_data.ndim == 3:
+#     exponent = jnp.expand_dims(exponent, axis=-1)
 
-  return media_transforms.apply_exponent_safe(data=gamma_adstock, exponent=exponent)
+#   return media_transforms.apply_exponent_safe(data=gamma_adstock, exponent=exponent)
 
 # ######################################################################################################
 
